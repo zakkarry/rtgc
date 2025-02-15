fa77d2b8a1e5e824a8c9909bd3068b37c2cc8bce
Michael Goodnow <mmgoodnow@gmail.com>
2025-02-15 13:54:17 -0800

    remove rawdir

--- symlinks-to-hardlinks.js
+++ symlinks-to-hardlinks.js
@@ -4,7 +4,6 @@ import { join, dirname, basename, relative, resolve } from "node:path";
 
 const options = {
 	"symlink-dir": {type: "string"},
-	"raw-dir": {type: "string"},
 	"output-dir": {type: "string"},
 	"execute": { type: "boolean", default: false }
 };
@@ -41,11 +40,6 @@ async function handleSymlink(symlinkPath) {
 		const symlinkRelativeFromSymlinkRoot = relative(symlinkDir, symlinkPath);
 		const relativeDir = dirname(symlinkRelativeFromSymlinkRoot);
 		const outputPath = join(outputDir, relativeDir, rawFilename);
-
-		if (!resolve(targetPath).startsWith(resolve(rawDir))) {
-			console.log(`Skipping symlink ${symlinkPath} because it is not in the raw directory.`);
-			return;
-		}
 
 		if (!execute) {
 			console.log(`[Dry Run] Would hardlink: ${targetPath} -> ${outputPath}`);
